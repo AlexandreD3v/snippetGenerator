b35da87633dd16b3d6532b976655e0977e8a637d
Alexandre J. Corrêa <alexandre.j.correa@hotmail.com>
2023-04-04 19:52:35 -0300

    Testing

--- src/vscode_snippet_generator.js
+++ src/vscode_snippet_generator.js
@@ -1,15 +1,20 @@
-const fs = require('fs');
-const readline = require('readline');
+const fs = require('fs'),
+    readline = require('readline'),
+    config = require('../sgconfig.json');
 
-fs.readdir("../in", (e, f) => {
+fs.readdir(config.input, (e, f) => {
     if (e) throw e;
 
+    var writeS = "";
+    if (config.buildFullSnippetFile) {
+        writeS = config.buildFullSnippetFile ? createWriteStreamObj("snnipets.json") : '';
+        writeS.write(`{ \n`);
+    }
     f.forEach((file) => {
-        var writeS = fs.createWriteStream("../out/" + file.replace(/\.ts$/, '.json')),
-            lines = [];
-
+        var lines = [];
+        writeS = config.buildFullSnippetFile ? writeS : createWriteStreamObj(file);
         readline.createInterface({
-            input: fs.createReadStream("../in/" + file)
+            input: fs.createReadStream(config.input + file)
         }).on('line', (l) => {
             lines.push('\"' + l.replace(/["]/g, '\\"') + '\", \n');
         }).on('close', () => {
@@ -17,7 +22,18 @@ fs.readdir("../in", (e, f) => {
             writeS.write(`\"prefix\": \"${file.replace(/\.ts$/, '')}\", \n`);
             writeS.write(`\"body\": [ \n ${lines.join("")}], \n`);
             writeS.write(`\"description\": \"${file.replace(/\.ts$/, '')}\", \n`);
+            writeS.write(`}, \n`);
+            if (config.buildFullSnippetFile && f.indexOf(file)+1 == f.length) writeS.write(`} \n`);
             console.log("Done!")
         })
     })
-});
+})
+
+function createWriteStreamObj(fileName) {
+    try {
+        return fs.createWriteStream(config.output + fileName.replace(/\.ts$/, '.json'));
+    } catch (e) {
+        console.log("Erro createWriteStream: " + e);
+        throw e;
+    }
+}
