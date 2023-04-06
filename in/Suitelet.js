2f94c960a1f7264383f5faa0b6b08bb96bcd8c02
Alexandre J. Corrêa <alexandre.j.correa@hotmail.com>
2023-04-05 21:29:56 -0300

    # Featured snippets: - Suitelet: blank Suitelet script with onRequest method - onRequest: Called when a Suitelet is accessed via a GET or POST request. The method can return a page object, a file object, or a string.

---
+++ in/Suitelet.js
@@ -0,0 +1,27 @@
+/**
+* @NApiVersion 2.x
+* @NScriptType Suitelet
+*/
+define([], function () {
+    function onRequest(context) {
+        switch (context) {
+            case "POST":
+                //TODO post
+                break;
+
+            case "GET":
+                //TODO get
+                break;
+
+            default:
+                //TODO default
+                break;
+
+        }
+    }
+
+    return {
+        onRequest: onRequest
+    };
+});
+
